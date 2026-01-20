3ca00bbacef3f2a2730c46d73bc2a5ce841b23d7
stichandlilo03-design <stichandlilo03@gmail.com>
2026-01-20 00:54:15 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import EmailSenderUltimate from './EmailSenderUltimate'
+import EmailSenderUltimate from './components/EmailSenderUltimate'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
